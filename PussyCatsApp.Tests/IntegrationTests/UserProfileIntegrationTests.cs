b6c60ef67fb654e9864fcb2c504e80bfc20d4be4
berintanemanuel <natiberintan3@gmail.com>
2026-04-25 14:32:54 +0300

    Fix integration test to use test database

--- PussyCatsApp.Tests/IntegrationTests/UserProfileIntegrationTests.cs
+++ PussyCatsApp.Tests/IntegrationTests/UserProfileIntegrationTests.cs
@@ -1,13 +1,14 @@
-﻿using PussyCatsApp.Services;
+﻿using PussyCatsApp.Configuration;
+using PussyCatsApp.Models;
+using PussyCatsApp.Repositories;
+using PussyCatsApp.Services;
+using PussyCatsApp.Tests.Infrastructure;
+using PussyCatsApp.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PussyCatsApp.Configuration;
-using PussyCatsApp.Models;
-using PussyCatsApp.Repositories;
-using PussyCatsApp.ViewModels;
 
 namespace PussyCatsApp.Tests.IntegrationTests
 {
@@ -17,13 +18,15 @@ namespace PussyCatsApp.Tests.IntegrationTests
         
         private IUserProfileRepository userProfileRepository;
         private IUserProfileService userProfileService;
+        private UserProfileViewModel userProfileViewModel;
 
         [TestInitialize]
         public void Setup()
         {
-            userProfileRepository = new UserProfileRepository(DatabaseConfiguration.GetConnectionString());// TO DO : move to test database
+            TestDatabaseHelper.ClearAllTables();
+            userProfileRepository = new UserProfileRepository(TestDatabaseHelper.ConnectionString);
             userProfileService = new UserProfileService(null, userProfileRepository);
-
+            userProfileViewModel = new UserProfileViewModel(userProfileService, new ImageStorageService(), new CompletenessService());
         }
 
         [TestMethod]
@@ -31,16 +34,14 @@ namespace PussyCatsApp.Tests.IntegrationTests
         [DataRow("INACTIVE",false)]
         public void ToggleAccountStatusCommand_ExistingUser_TogglesCorrectly(string initialAccountStatus, bool FinalAccountStatus)
         {
-            // Arrange
-            var viewModel = new UserProfileViewModel(userProfileService, null, null);
-            Assert.IsFalse(userProfileRepository.GetProfileById(1) == null);
-            userProfileRepository.UpdateAccountStatus(1, initialAccountStatus);
+            int userId = TestDatabaseHelper.InsertUser();
+            Assert.IsFalse(userProfileRepository.GetProfileById(userId) == null);
+            userProfileRepository.UpdateAccountStatus(userId, initialAccountStatus);
 
-            viewModel.ToggleAccountStatusCommand();
+            userProfileViewModel.ToggleAccountStatusCommand();
 
-            var userWithId1 = userProfileService.GetProfile(1);
-            Assert.AreEqual(FinalAccountStatus, userWithId1.ActiveAccount);
-
+            UserProfile userWithGivenId = userProfileService.GetProfile(userId);
+            Assert.AreEqual(FinalAccountStatus, userWithGivenId.ActiveAccount);
         }
 
         [TestCleanup]
